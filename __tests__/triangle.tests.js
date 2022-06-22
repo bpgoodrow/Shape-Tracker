58cfab85c6cc41ff28a1c8d2004775cf4ebc5462
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-22 13:13:37 -0700

    pass 3 tests successfully
    Joseph Jack <joseph.w.jack@gmail.com>

--- __tests__/triangle.tests.js
+++ __tests__/triangle.tests.js
@@ -8,24 +8,12 @@ describe('Triangle', () => {
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
-});
-
-test('should correctly determine whether three lengths are not a triangle', () => {
-  const notTriangle = new Triangle(3,9,22);
-  expect(notTriangle.checkType()).toEqual("not a triangle");
-});
-
-test('should correctly determine whether three lengths make a scalene triangle', () => {
-  const scalTriangle = new Triangle(4,5,7)
-  expect(scalTriangle.checkType()).toEqual("scalene triangle");
-});
-
-test('should correctly determine wehter three lengths make an isosceles triangle', () => {
-  const isocTriangle = new Triangle(5,5,7)
-  expect(isocTriangle.checkType()).toEqual("isosceles triangle");
-});
-
-test('should correctly determine whether three lengths make an equilateral triangle', () => {
-  const equiTriangle = new Triangle(5,5,5)
-  expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  test('should return an object that is not a triangle', () => {
+    const notTriangle = new Triangle(2, 4, 40);
+    expect(notTriangle.checkType()).toEqual('not a triangle');
+  });
+  test('should return equilateral if triangle object has three sides of same length', () => {
+    const triangle = new Triangle(5, 5, 5);
+    expect(triangle.checkType()).toEqual('equilateral');
+  });
 });
--- src/triangle.js
+++ src/triangle.js
@@ -1,4 +1,3 @@
-
 export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
@@ -6,13 +5,9 @@ export default function Triangle(side1, side2, side3) {
 }
 
 Triangle.prototype.checkType = function() {
-  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-    return "not a triangle";
-  } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
-    return "scalene triangle";
-  } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
-      return "equilateral triangle";
+  if((this.side1 === this.side2) && (this.side2 === this.side3) && (this.side3 === this.side1)) {
+    return "equilateral";
   } else {
-    return "isosceles triangle";
-  }
+  return "not a triangle";
+  };
 };
