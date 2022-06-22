cce0233224e2d58406234fb0ae92cbb3f5e2c9d9
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-22 13:47:19 -0700

    test 5 successful
    Joseph Jack <joseph.w.jack@gmail.com>

--- __tests__/triangle.tests.js
+++ __tests__/triangle.tests.js
@@ -9,11 +9,19 @@ describe('Triangle', () => {
     expect(triangle.side3).toEqual(5);
   });
   test('should return an object that is not a triangle', () => {
-    const notTriangle = new Triangle(2, 4, 40);
+    const notTriangle = new Triangle(2, 8, 40);
     expect(notTriangle.checkType()).toEqual('not a triangle');
   });
   test('should return equilateral if triangle object has three sides of same length', () => {
     const triangle = new Triangle(5, 5, 5);
     expect(triangle.checkType()).toEqual('equilateral');
+  });
+  test('should return scalene triangle if no sides are the same length', () => {
+    const triangle = new Triangle(4, 3, 5);
+    expect(triangle.checkType()).toEqual('scalene');
+  });
+  test('should return isosceles if two sides of the triangle are the same length', () => {
+    const triangle = new Triangle(5, 5, 6);
+    expect(triangle.checkType()).toEqual('isosceles');
   });
 });
